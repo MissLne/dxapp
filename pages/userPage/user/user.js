900bffc82b7a33b4d3b438171ce1c26e6b3d4165
MissLne <7351015+misslne@user.noreply.gitee.com>
2021-04-08 12:28:35 +0800

    wewe

--- pages/userPage/user/user.js
+++ pages/userPage/user/user.js
@@ -1,66 +1,35 @@
-// pages/userPage/user/user.js
+const request = require('../../../request/api')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    topBarMargin: 0,
+    topBarHeight: 0,
+    userMessageObject: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      topBarMargin: (wx.getMenuButtonBoundingClientRect().top) * 2,
+      topBarHeight: (wx.getMenuButtonBoundingClientRect().height) * 2,
+    })
+    this.showUserMaterial()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  showUserMaterial() {
+    let obj = {
+      id: wx.getStorageSync('id')
+    }
+    request.showUserMessge(obj)
+    .then(res => {
+      console.log(res.data)
+      this.setData({
+        userMessageObject: res.data
+      })
+    })
   }
 })
