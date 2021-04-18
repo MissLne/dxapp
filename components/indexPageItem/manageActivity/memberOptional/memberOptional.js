3cf811a647bd01fe7565783e565ae1ced71e303d
MissLne <7351015+misslne@user.noreply.gitee.com>
2021-04-18 12:55:13 +0800

    zhuanfa

---
+++ components/indexPageItem/manageActivity/memberOptional/memberOptional.js
@@ -0,0 +1,32 @@
+// components/indexPageItem/manageActivity/memberOptional/memberOptional.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    optional: Array
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    baseMaterial: []
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  ready: function() {
+    setTimeout(() => {
+      console.log(this.properties.optional[0])
+      this.setData({
+        baseMaterial: this.properties.optional
+      })
+    },300)
+    
+  },
+  methods: {
+    
+  }
+})
--- pages/activityPage/manage/memberMessage/memberMessage.js
+++ pages/activityPage/manage/memberMessage/memberMessage.js
@@ -6,28 +6,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    memberObj: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // console.log(parseInt(options.activityId)
     this.showDetail(options)
+    wx.showShareMenu({
+      withShareTicket: true
+    })
   },
-
-
-  showDetail(reqObj) {
-    let activityId = parseInt(reqObj.activityId)
-    let obj = {
-      activityId: activityId,
-      mId: reqObj.mId.substr(1,reqObj.mId.length - 2)
+  onShareAppMessage: function (ops) {
+    if (ops.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(ops.target)
+      console.log(this.data.id)
     }
-    console.log(reqObj.mId.substr(1,reqObj.mId.length - 2))
-    request.showMemberDetail(obj)
-      .then(res => {
-        console.log(res)
-      })
+    return {
+      title: '标签l',
+      path: '/pages/loginPage/register/register', //这里设定都是以"/page"开头,并拼接好传递的参数
+      success: function (res) {
+        // 转发成功
+        console.log(res);
+        console.log("转发成功:" + JSON.stringify(res));
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log("转发失败:" + JSON.stringify(res));
+      }
+    }
+ 
+    },
+showDetail(reqObj) {
+  let activityId = parseInt(reqObj.activityId)
+  let obj = {
+    activityId: activityId,
+    mId: reqObj.mId.substr(1, reqObj.mId.length - 2)
   }
+  request.showMemberDetail(obj)
+    .then(res => {
+      console.log(res)
+      this.setData({
+        memberObj: res.data
+      })
+    })
+}
 })
